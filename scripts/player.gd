ff48b3e84dd5774f7bfd966ff5c8f5e42ecd0a58
Alessio De Nicolò <115568998+AleDeNic@users.noreply.github.com>
2024-06-24 10:43:44 +0200

    refactor -> player movement script improved
    now the "reset to phone" function works with move_and_collide()

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 @onready var window: Area2D = $"../Window"
 
 @export var move_speed: float = 800.0  # Speed of the player when moving towards a target
+@export var reset_speed: float = 2500.0
 @export var mouse_sensitivity: float = 25  # Adjust this to change how much camera moves with mouse
 var last_mouse_pos: Vector2
 
@@ -16,36 +17,30 @@ func _ready() -> void:
 	last_mouse_pos = get_viewport().get_mouse_position()
 
 func _process(_delta: float) -> void:
-	# Get the current mouse position
+	handle_mouse_movement()
+	reset_to_phone(phone.position)
+
+func handle_mouse_movement() -> void:
 	var current_mouse_pos: Vector2 = get_viewport().get_mouse_position()
-	
-	# Calculate the mouse movement relative to the center
-	var viewport_size: Vector2   = get_viewport().get_visible_rect().size
+	var viewport_size: Vector2 = get_viewport().get_visible_rect().size
 	var center_position: Vector2 = viewport_size / 2
-	var mouse_movement: Vector2  = current_mouse_pos - center_position
-	
-	reset_to_phone()
-	
-	# Reset the mouse to the center of the screen
+
+	var mouse_movement: Vector2 = current_mouse_pos - center_position
 	reset_mouse_to_center()
-	
-	# Calculate the new motion based on mouse movement
-	var motion: Vector2 = mouse_movement * mouse_sensitivity
-	velocity = motion
+
+	velocity = mouse_movement * mouse_sensitivity
 	move_and_slide()
 
-# Function to reset the mouse cursor to the center of the screen
 func reset_mouse_to_center():
-	# Get the center of the viewport
-	var viewport_size: Vector2   = get_viewport().get_visible_rect().size
+	var viewport_size: Vector2 = get_viewport().get_visible_rect().size
 	var center_position: Vector2 = viewport_size / 2
-	
-	# Set the mouse position to the center of the screen
 	Input.warp_mouse(center_position)
 
-func reset_to_phone() -> void:
+func reset_to_phone(phone_position: Vector2):
 	if game_manager.attention_span < 0:
-		# TO CHANGE TO MOVE_AND_COLLIDE!!!
-		position = phone.position
-		#move_and_collide(velocity)
-		return  # Early return to avoid further processing
+		var direction: Vector2 = (phone_position - position).normalized()
+		velocity = direction * reset_speed * get_physics_process_delta_time()
+		move_and_collide(velocity)
+
+		if position.distance_to(phone_position) < 1.0:
+			velocity = Vector2.ZERO
