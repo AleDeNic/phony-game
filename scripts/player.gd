8644d128516723ee504e85e4e0522a45aa895c20
Alessio De Nicolò <115568998+AleDeNic@users.noreply.github.com>
2024-06-25 11:48:02 +0200

    feature -> improved movement
    now the transition between the movement speeds is smooth

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,37 +5,51 @@ extends CharacterBody2D
 @onready var asuka: Area2D = $"../Asuka"
 @onready var window: Area2D = $"../Window"
 
-@export var move_speed: float = 800.0  # Speed of the player when moving towards a target
 @export var reset_speed: float = 2500.0
-@export var mouse_sensitivity: float = 35.0  # Adjust this to change how much camera moves with mouse
-@export var mouse_phone_sensitivity: float = 0.7
+@export var mouse_sensitivity: float = 45.0  # Normal sensitivity
+@export var mouse_phone_sensitivity: float = 0.7  # Sensitivity when zoomed in
+@export var transition_speed: float = 5.0  # Speed of the sensitivity transition
+
 var last_mouse_pos: Vector2
+var current_sensitivity: float  # Current sensitivity being used
+var target_sensitivity: float  # Target sensitivity to transition to
 
 func _ready() -> void:
 	# Initialize last mouse position
-	position = phone.position
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	reset_mouse_to_center()
 	last_mouse_pos = get_viewport().get_mouse_position()
+	
+	# Initialize sensitivities
+	current_sensitivity = mouse_sensitivity
+	target_sensitivity = mouse_sensitivity
 
 func _process(_delta: float) -> void:
-	handle_mouse_movement()
+	handle_mouse_movement(_delta)
 	reset_to_phone(phone.position)
 
-func handle_mouse_movement() -> void:
+func handle_mouse_movement(delta: float) -> void:
+	# Update current sensitivity towards the target sensitivity
+	current_sensitivity = lerp(current_sensitivity, target_sensitivity, delta * transition_speed)
+	
 	var current_mouse_pos: Vector2 = get_viewport().get_mouse_position()
 	var viewport_size: Vector2 = get_viewport().get_visible_rect().size
 	var center_position: Vector2 = viewport_size / 2
 	var mouse_movement: Vector2 = current_mouse_pos - center_position
+	
 	calculate_velocity(mouse_movement)
 	move_and_slide()
 
-func calculate_velocity(mouse_movement) -> void:
-	if phone.is_zooming_in == false:
-		velocity = mouse_movement * mouse_sensitivity
-		reset_mouse_to_center()
+func calculate_velocity(mouse_movement: Vector2) -> void:
+	if phone.is_zooming_in:
+		transition_speed = 50
+		target_sensitivity = mouse_phone_sensitivity
 	else:
-		velocity = mouse_movement * mouse_phone_sensitivity
+		transition_speed = 5
+		target_sensitivity = mouse_sensitivity
+		reset_mouse_to_center()
+
+	velocity = mouse_movement * current_sensitivity
 
 func reset_mouse_to_center():
 	var viewport_size: Vector2 = get_viewport().get_visible_rect().size
