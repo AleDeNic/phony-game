f088175aa2d96ceb0aca4602b44c602d40132339
Samuele Castiglia <samuele.castiglia@gntechonomy.co>
2024-07-01 14:40:08 +0200

    feat-wip: Gestione multiple timeline

--- scripts/asuka.gd
+++ scripts/asuka.gd
@@ -25,22 +25,26 @@ func _on_area_entered(_area: Area2D) -> void:
 	camera.start_zoom(camera.asuka_zoom_value, camera.asuka_zoom_speed)
 	await get_tree().create_timer(0.3).timeout
 	eyes_sprite.frame = 1
+	print("Entered Asuka area: ", game_manager.timelines)
 
 func _on_area_exited(_area: Area2D) -> void:
 	timer.stop()
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	game_manager.handle_timeline("asuka")
+	game_manager.handle_timeline("asuka", true)
+	if Dialogic.Styles.get_layout_node():
+		Dialogic.Styles.get_layout_node().hide()
 	is_zooming_in = false
 	asuka_scale(scale_down_speed)
 	camera.start_zoom(camera.default_zoom_value, camera.reset_zoom_speed)
 	await get_tree().create_timer(0.3).timeout
 	eyes_sprite.frame = 0
+	print("Exited Asuka area: ", game_manager.timelines)
 
 func _on_asuka_animation_animation_finished(_anim_name: StringName) -> void:
 	if is_zooming_in == false:
 		asuka_sprite.z_index = 0
 
-func asuka_scale(scale_speed) -> void:
+func asuka_scale(scale_speed: float) -> void:
 	asuka_animation.speed_scale = scale_speed
 	if is_zooming_in:
 		asuka_animation.play("scale")
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,6 @@
 extends Node
 
 # TODO: Investigate why the fuck this does have to be manual. Thanks
-var timeline_started: bool
 
 @export_group("Tasks values")
 @export var phone_stress_heal: float = 0.3
@@ -12,25 +11,40 @@ var timeline_started: bool
 @export var max_stress: float = 30.0
 @export var max_battery: float = 180.0
 
+var timelines: Dictionary = {"asuka": false, "window": false}
+
+var object: Object = {Object: {started: false}}
+
 func _ready() -> void:
-	timeline_started = false
+	for timeline in timelines:
+		timelines[timeline] = false
+	print("Timelines initialized: ", timelines)
 	
-func handle_timeline(timeline) -> void:
-	if timeline_started:
-		if Dialogic.paused:
-			Dialogic.paused = false
-			if Dialogic.Styles.get_layout_node():
-				Dialogic.Styles.get_layout_node().show()
-				print(timeline + " timeline resumed")
-		else:
-			Dialogic.paused = true
+func handle_timeline(timeline: String, reset: bool = false) -> void:
+	print("Handling timeline: ", timeline)
+	if timelines.has(timeline):
+		if reset:
+			timelines[timeline] = false
 			if Dialogic.Styles.get_layout_node():
 				Dialogic.Styles.get_layout_node().hide()
-				print(timeline + " timeline paused")
+			print(timeline + " timeline reset | Timelines: ", timelines)
+		elif timelines[timeline]:
+			if Dialogic.paused:
+				Dialogic.paused = false
+				if Dialogic.Styles.get_layout_node():
+					Dialogic.Styles.get_layout_node().show()
+					print(timeline + " timeline resumed | Timelines: ", timelines)
+			else:
+				Dialogic.paused = true
+				if Dialogic.Styles.get_layout_node():
+					Dialogic.Styles.get_layout_node().hide()
+					print(timeline + " timeline paused | Timelines: ", timelines)
+		else:
+			Dialogic.start(timeline)
+			Dialogic.process_mode = Node.PROCESS_MODE_ALWAYS
+			timelines[timeline] = true
+			if Dialogic.Styles.get_layout_node():
+				Dialogic.Styles.get_layout_node().show()
+			print(timeline + " timeline started | Timelines: ", timelines)
 	else:
-		Dialogic.start(timeline)
-		Dialogic.process_mode = Node.PROCESS_MODE_ALWAYS
-		timeline_started = true
-		if Dialogic.Styles.get_layout_node():
-			Dialogic.Styles.get_layout_node().show()
-			print(timeline + " timeline started")
+		print("Timeline ", timeline, " does not exist in timelines dictionary.")
