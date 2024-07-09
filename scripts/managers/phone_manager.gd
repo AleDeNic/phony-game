409472c12c7bbad94f88df730e0188901e4fcc63
Samuele Castiglia <samuele.castiglia.98@gmail.com>
2024-07-09 15:20:01 +0200

    refactor: Moved managers into managers folder and re-linked autoload resources

---
+++ scripts/managers/phone_manager.gd
@@ -0,0 +1,75 @@
+extends Node
+
+enum PhoneState { OFF, APPS, OPTIONS, CAMERA, CHATS, ASUKACHAT }
+
+@onready var phone_state: PhoneState = PhoneState.OFF
+
+# ----- INITIALIZERS -----
+# ...
+
+# ----- STATE SETTERS -----
+
+func set_phone_state(new_state: PhoneState) -> void:
+	phone_state = new_state
+	print("Phone -> ", get_phone_state_value())
+	
+func set_phone_off() -> void:
+	set_phone_state(PhoneState.OFF)
+
+func set_phone_in_apps() -> void:
+	set_phone_state(PhoneState.APPS)
+	
+func set_phone_in_options() -> void:
+	set_phone_state(PhoneState.OPTIONS)
+	
+func set_phone_in_camera() -> void:
+	set_phone_state(PhoneState.CAMERA)
+	
+func set_phone_in_chats() -> void:
+	set_phone_state(PhoneState.CHATS)
+	
+func set_phone_in_asukachat() -> void:
+	set_phone_state(PhoneState.ASUKACHAT)
+
+			
+# ----- STATE GETTERS -----
+
+func get_phone_state() -> PhoneState:
+	return phone_state
+
+func get_phone_state_value():
+	match phone_state:
+		PhoneState.OFF:
+			return "OFF"
+		PhoneState.APPS:
+			return "APPS"
+		PhoneState.OPTIONS:
+			return "OPTIONS"
+		PhoneState.CAMERA:
+			return "CAMERA"
+		PhoneState.CHATS:
+			return "CHATS"
+		PhoneState.ASUKACHAT:
+			return "ASUKA CHAT"
+			
+func is_phone_off() -> bool:
+	return phone_state == PhoneState.OFF
+	
+func is_phone_in_apps() -> bool:
+	return phone_state == PhoneState.APPS
+	
+func is_phone_in_options() -> bool:
+	return phone_state == PhoneState.OPTIONS
+	
+func is_phone_in_camera() -> bool:
+	return phone_state == PhoneState.CAMERA
+	
+func is_phone_in_chats() -> bool:
+	return phone_state == PhoneState.CHATS
+	
+func is_phone_in_asukachat() -> bool:
+	return phone_state == PhoneState.ASUKACHAT
+	
+func is_battery_active() -> bool:
+	return phone_state not in [PhoneState.OFF, PhoneState.OPTIONS]
+	
