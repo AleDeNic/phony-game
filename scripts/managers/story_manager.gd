fa2d2e544c29a6e539385b56ad7d7c41623d00ca
Samuele Castiglia <samuele.castiglia.98@gmail.com>
2024-07-09 17:54:33 +0200

    feat: Dynamic balloons in StoryManager

--- scripts/managers/story_manager.gd
+++ scripts/managers/story_manager.gd
@@ -3,6 +3,10 @@ extends Node
 var active_balloon: Node = null
 var current_dialogue_area: Area2D = null
 
+### If you need to add a new balloon, copy the scene below. Remember that if you need
+### to change the balloon's behaviour, you will need to create another script under scripts/balloons.
+@onready var default_balloon: String = "res://scenes/balloons/default_balloon.tscn"
+# @onready var phone_balloon: String = "path/to/phone_balloon.tscn"
 
 # ----- INITIALIZATION AND PHYSICS -----
 
@@ -11,10 +15,10 @@ func _ready() -> void:
 
 # ----- DIALOGUES -----
 
-func start_dialogue(dialogue_resource: Resource, start_from: String, dialogue_area: Area2D) -> void:
+func start_dialogue(dialogue_scene: String, dialogue_resource: Resource, start_from: String, dialogue_area: Area2D) -> void:
 	print("Dialogue ", dialogue_resource.resource_path, " started")
 	
-	active_balloon = DialogueManager.show_dialogue_balloon(dialogue_resource, start_from)
+	active_balloon = DialogueManager.show_dialogue_balloon_scene(dialogue_scene, dialogue_resource, start_from)
 	current_dialogue_area = dialogue_area
 
 func end_dialogue() -> void:
