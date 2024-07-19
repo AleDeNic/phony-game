59d3f78517179ba52c8092d6941870c5ddc14cb3
Samuele Castiglia <samuele.castiglia@gntechonomy.co>
2024-07-19 12:41:44 +0200

    refactor: Phases

--- scripts/managers/phase_manager.gd
+++ scripts/managers/phase_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
-enum Phase { SPLASH, PROLOGUE, MIDDLE, END, CREDITS }
-@onready var phase_state: Phase = Phase.PROLOGUE
+enum State { SPLASH, PROLOGUE, MIDDLE, END, CREDITS }
+@onready var phase_state: State = State.PROLOGUE
 @onready var points: int = 0
 @onready var threshold: int = 15
 
@@ -11,105 +11,132 @@ var probability: float = 20.0
 # ----- POINTS -----
 
 func get_points() -> int:
-    return points
+	return points
 
 
 func set_points(new_points: int) -> void:
-    points = new_points
-    print("Points -> ", points)
+	points = new_points
+	print("Points -> ", points)
 
 
 func add_points(new_points: int) -> void:
-    points += new_points
-    print("Points -> ", points)
+	points += new_points
+	print("Points -> ", points)
 
 
 func get_threshold() -> int:
-    return threshold
+	return threshold
 
 
 func set_threshold(new_threshold: int) -> void:
-    threshold = new_threshold
-    print("Threshold -> ", threshold)
+	threshold = new_threshold
+	print("Threshold -> ", threshold)
 
 
 # TODO: This is a placeholder. Implement a proper check
 func check_threshold() -> bool:
-    match points:
-        threshold:
-            return true
-        _:
-            return false
+	match points:
+		threshold:
+			return true
+		_:
+			return false
 
 
 # ----- PHASE -----
-func set_phase(new_phase: Phase) -> void:
-    phase_state = new_phase
-    print("Phase -> ", get_phase_value())
+func set_phase(new_phase: State) -> void:
+	phase_state = new_phase
+	print("Phase -> ", get_phase_value())
 
 
-func get_phase() -> Phase:
-    return phase_state
+func get_phase() -> State:
+	return phase_state
 
 
 func get_phase_value() -> String:
-    match phase_state:
-        Phase.SPLASH:
-            return "SPLASH"
-        Phase.PROLOGUE:
-            return "PROLOGUE"
-        Phase.MIDDLE:
-            return "MIDDLE"
-        Phase.END:
-            return "END"
-        Phase.CREDITS:
-            return "CREDITS"
-        _:
-            return "UNKNOWN"
+	match Phases.get_phase():
+		Phases.is_splash():
+			return "SPLASH"
+		Phases.is_prologue():
+			return "PROLOGUE"
+		Phases.is_middle():
+			return "MIDDLE"
+		Phases.is_end():
+			return "END"
+		Phases.is_credits():
+			return "CREDITS"
+		_:
+			return "UNKNOWN"
+
+
+func set_splash() -> void:
+	set_phase(Phases.State.SPLASH)
+
+
+func set_prologue() -> void:
+	set_phase(Phases.State.PROLOGUE)
+
+
+func set_middle() -> void:
+	set_phase(Phases.State.MIDDLE)
+
+
+func set_end() -> void:
+	set_phase(Phases.State.END)
+
+
+func set_credits() -> void:
+	set_phase(Phases.State.CREDITS)
+
 
 func is_splash() -> bool:
-    return phase_state == Phase.SPLASH
+	return phase_state == State.SPLASH
+
 
 func is_prologue() -> bool:
-    return phase_state == Phase.PROLOGUE
+	return phase_state == State.PROLOGUE
+
 
 func is_middle() -> bool:
-    return phase_state == Phase.MIDDLE
+	return phase_state == State.MIDDLE
+
 
 func is_end() -> bool:
-    return phase_state == Phase.END
+	return phase_state == State.END
+
 
 func is_credits() -> bool:
-    return phase_state == Phase.CREDITS
+	return phase_state == State.CREDITS
+
 
 # TODO: Funny. Very funny. But I'm not laughing.
 func advance() -> void:
-    match phase_state:
-        Phase.SPLASH:
-            set_phase(Phase.PROLOGUE)
-            print("probability: ", probability)
-        Phase.PROLOGUE:
-            set_phase(Phase.MIDDLE)
-            probability += 20.0
-            print("probability: ", probability)
-        Phase.MIDDLE:
-            set_phase(Phase.END)
-            probability += 20.0
-            print("probability: ", probability)
-        Phase.END:
-            set_phase(Phase.CREDITS)
-        Phase.CREDITS:
-            pass
+	match Phases.get_phase():
+		Phases.is_splash():
+			set_prologue()
+			print("probability: ", probability)
+		Phases.is_prologue():
+			set_middle()
+			probability += 20.0
+			print("probability: ", probability)
+		Phases.is_middle():
+			set_end()
+			probability += 20.0
+			print("probability: ", probability)
+		Phases.is_end():
+			set_credits()
+		Phases.is_credits():
+			pass
+
 
 # ----- UTILS -----
 
 func can_dialogue_spawn() -> bool:
-    if Story.angry_dialogue_active:
-        print("Angry dialogue active, skipping spawn check")
-        return false
+	if Story.angry_dialogue_active:
+		print("Angry dialogue active, skipping spawn check")
+		return false
 
-    print("Probability: ", probability)
-    var frankiePie: float = RandomNumberGenerator.new().randf_range(0.0, 100.0)
+	print("Probability: ", probability)
+	var frankiePie: float = RandomNumberGenerator.new().randf_range(0.0, 100.0)
 
-    print("frankiePie:fereacotr: _> refunct _> YouCanNot(new Anatichember) roooxane (toto) i lvoe tprogramign: -> ", frankiePie)
-    return frankiePie <= probability
+	print("frankiePie:fereacotr: _> refunct _> YouCanNot(new Anatichember) roooxane (toto) i lvoe tprogramign: -> ", frankiePie)
+	return frankiePie <= probability
--- scripts/player.gd
+++ scripts/player.gd
@@ -20,100 +20,100 @@ var focus_speed: float
 # ----- INITIALIZATION -----
 
 func _ready() -> void:
-    add_to_group("player")
-    setup_viewport()
-    current_speed = default_speed
-    focus_speed = focus_speed_phone
+	add_to_group("player")
+	setup_viewport()
+	current_speed = default_speed
+	focus_speed = focus_speed_phone
 
 
 func _physics_process(_delta: float) -> void:
-    check_viewport()
-    update_player(_delta)
+	check_viewport()
+	update_player(_delta)
 
 
 ## ----- MOVEMENT -----
 
 func handle_movement(delta: float) -> void:
-    var movement_vector: Vector2 = get_movement_input()
-    target_speed = default_speed
-    reset_mouse_to_center()
-    current_speed = lerp(current_speed, target_speed, delta * transition_speed)
-    velocity = velocity.lerp(movement_vector * current_speed, delta * transition_speed)
-    move_and_slide()
+	var movement_vector: Vector2 = get_movement_input()
+	target_speed = default_speed
+	reset_mouse_to_center()
+	current_speed = lerp(current_speed, target_speed, delta * transition_speed)
+	velocity = velocity.lerp(movement_vector * current_speed, delta * transition_speed)
+	move_and_slide()
 
 
 func focus(delta: float) -> void:
-    var movement_vector: Vector2 = (target_position - global_position).normalized()
-    if global_position.distance_to(target_position) >= 5.0:
-        target_speed = focus_speed
-        move(delta, movement_vector)
-    else:
-        current_speed = 0.0
+	var movement_vector: Vector2 = (target_position - global_position).normalized()
+	if global_position.distance_to(target_position) >= 5.0:
+		target_speed = focus_speed
+		move(delta, movement_vector)
+	else:
+		current_speed = 0.0
 
 
 func unfocus(delta: float) -> void:
-    var movement_vector: Vector2 = get_movement_input()
-    target_speed = default_speed
-    move(delta, movement_vector)
-    reset_mouse_to_center()
+	var movement_vector: Vector2 = get_movement_input()
+	target_speed = default_speed
+	move(delta, movement_vector)
+	reset_mouse_to_center()
 
 
 func move(delta: float, movement_vector: Vector2) -> void:
-    current_speed = lerp(current_speed, target_speed, delta * transition_speed)
-    velocity = velocity.lerp(movement_vector * current_speed, delta * transition_speed)
-    move_and_slide()
+	current_speed = lerp(current_speed, target_speed, delta * transition_speed)
+	velocity = velocity.lerp(movement_vector * current_speed, delta * transition_speed)
+	move_and_slide()
 
 
 func set_target(target_global_position: Vector2, speed: float) -> void:
-    target_position = Vector2(target_global_position)
-    focus_speed = speed
+	target_position = Vector2(target_global_position)
+	focus_speed = speed
 
 
 ## ----- INPUT -----
 
 func get_movement_input() -> Vector2:
-    if not viewport:
-        return Vector2.ZERO
-    var mouse_pos: Vector2       = viewport.get_mouse_position()
-    var center: Vector2          = viewport.get_visible_rect().size / 2
-    var movement_vector: Vector2 = mouse_pos - center
-    if movement_vector.length() < dead_zone_radius:
-        return Vector2.ZERO
-    else:
-        return movement_vector
+	if not viewport:
+		return Vector2.ZERO
+	var mouse_pos: Vector2       = viewport.get_mouse_position()
+	var center: Vector2          = viewport.get_visible_rect().size / 2
+	var movement_vector: Vector2 = mouse_pos - center
+	if movement_vector.length() < dead_zone_radius:
+		return Vector2.ZERO
+	else:
+		return movement_vector
 
 
 func reset_mouse_to_center() -> void:
-    if viewport:
-        viewport.warp_mouse(viewport.get_visible_rect().size / 2)
+	if viewport:
+		viewport.warp_mouse(viewport.get_visible_rect().size / 2)
 
 
 # ----- UTILS -----
 
 func setup_viewport() -> void:
-    viewport = get_viewport()
-    if viewport:
-        reset_mouse_to_center()
+	viewport = get_viewport()
+	if viewport:
+		reset_mouse_to_center()
 
 
 func check_viewport() -> void:
-    if not viewport:
-        setup_viewport()
-        return
+	if not viewport:
+		setup_viewport()
+		return
 
 
 func update_player(_delta: float) -> void:
-    match Player.get_state():
-        Player.is_focusing_on_asuka(), Player.is_focused_on_asuka():
-            camera.set_camera_zoom(camera.asuka_zoom_value, camera.asuka_zoom_speed)
-            handle_movement(_delta)
-        Player.is_focusing_on_phone(), Player.is_focused_on_phone():
-            camera.set_camera_zoom(camera.phone_zoom_value, camera.phone_zoom_speed)
-            focus(_delta)
-        Player.is_free(), Player.is_unfocusing():
-            camera.set_camera_zoom(camera.default_zoom_value, camera.reset_zoom_speed)
-            target_speed = 0.0
-            current_speed = 0.0
-            velocity = Vector2.ZERO
-        Player.is_dialogue_paused():
-            pass
+	match Player.get_state():
+		Player.is_focusing_on_asuka(), Player.is_focused_on_asuka():
+			camera.set_camera_zoom(camera.asuka_zoom_value, camera.asuka_zoom_speed)
+			handle_movement(_delta)
+		Player.is_focusing_on_phone(), Player.is_focused_on_phone():
+			camera.set_camera_zoom(camera.phone_zoom_value, camera.phone_zoom_speed)
+			focus(_delta)
+		Player.is_free(), Player.is_unfocusing():
+			camera.set_camera_zoom(camera.default_zoom_value, camera.reset_zoom_speed)
+			target_speed = 0.0
+			current_speed = 0.0
+			velocity = Vector2.ZERO
+		Player.is_dialogue_paused():
+			pass
